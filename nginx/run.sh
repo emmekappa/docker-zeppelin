ec1b9b1a0ae36383d1481f8304fd05d64f221bdb
Michele Cantelli <michele.cantelli@yoox.com>
2015-09-25 22:42:20 +0200

    websocket

--- nginx/run.sh
+++ nginx/run.sh
@@ -7,6 +7,8 @@ env
 # Preprend the upstream configuration
 sed -i s/ZEPPELIN_PORT_8080_TCP_ADDR/${ZEPPELIN_PORT_8080_TCP_ADDR}/g /etc/nginx/nginx.conf 
 sed -i s/ZEPPELIN_PORT_8080_TCP_PORT/${ZEPPELIN_PORT_8080_TCP_PORT}/g /etc/nginx/nginx.conf
+sed -i s/ZEPPELIN_PORT_8081_TCP_ADDR/${ZEPPELIN_PORT_8081_TCP_ADDR}/g /etc/nginx/nginx.conf
+sed -i s/ZEPPELIN_PORT_8081_TCP_PORT/${ZEPPELIN_PORT_8081_TCP_PORT}/g /etc/nginx/nginx.conf
 
 # log result
 cat /etc/nginx/nginx.conf
